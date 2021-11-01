e2e67bb1d0771c2e03d9f6f0221a0607cd101ca0
Hepheir <hepheir@gmail.com>
2021-11-01 15:04:50 +0900

    HitBox에서 버튼 제거. 오직 onclick 기능만 지원하는 틀로서만 기능하도록 함

--- src/components/HitBox/index.jsx
+++ src/components/HitBox/index.jsx
@@ -4,24 +4,13 @@ import "./style.css";
 
 
 function HitBox(props) {
-    const { children, onClick, placeholder } = props;
-
+    const { children, onClick } = props;
     return (
         <div className="hit-box" onClick={onClick}>
             {children}
-            <HitBoxButton>
-                {placeholder ?? '+'}
-            </HitBoxButton>
         </div>
     );
 }
 
-
-function HitBoxButton(props) {
-    const { children } = props;
-    return (
-        <div className="hit-box-button">{children}</div>
-    );
-}
 
 export default HitBox;
