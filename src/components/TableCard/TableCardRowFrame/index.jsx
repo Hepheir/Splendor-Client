0a1d7b910217f710f62c6c8b79a9d5d11eac557c
Hepheir <hepheir@gmail.com>
2021-11-08 12:18:49 +0900

    components, layout: 카드 프레임의 자연스런 배치를 위해 수정

--- src/components/TableCard/TableCardRowFrame/index.jsx
+++ src/components/TableCard/TableCardRowFrame/index.jsx
@@ -4,7 +4,11 @@ import "./style.css";
 function TableCardRowFrame(props) {
     const { children } = props;
     return (
-        <div className="table-card-row-frame">{children}</div>
+        <div className="table-card-row-frame-container">
+            <div className="table-card-row-frame">
+                {children}
+            </div>
+        </div>
     );
 }
 
