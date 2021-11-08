ed8d75391b0208db90e4a6d697f2369aeb15ca39
Hepheir <hepheir@gmail.com>
2021-11-08 12:15:51 +0900

    components/Action: ActionButton 컴포넌트에 isActive 속성 추가

--- src/components/Action/ActionButton/index.jsx
+++ src/components/Action/ActionButton/index.jsx
@@ -1,10 +1,15 @@
+import classNames from "classnames";
+
 import "./style.css";
 
 
 function ActionButton(props) {
-    const { children } = props;
+    const { children, isActive } = props;
     return (
-        <div className="action-button">
+        <div className={classNames(
+            "action-button",
+            {"is-active": isActive},
+        )}>
             {children}
         </div>
     );
