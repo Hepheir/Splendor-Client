d23ac41700ac3f00357da88449148d8a9d971264
Hepheir <hepheir@gmail.com>
2021-11-09 16:09:50 +0900

    components/TableCard: 일부 컴포넌트 삭제
    삭제된 TableCard, TableDrawpile은 Game 컴포넌트에서 리펙터링 된 Card 컴포넌트를 통해 적용할 수 있도록 변경

--- src/components/TableCard/index.jsx
+++ src/components/TableCard/index.jsx
@@ -1,13 +1,9 @@
 import TableCardFrame from "./TableCardFrame";
 import TableCardRowFrame from "./TableCardRowFrame";
 import TableCardColumnFrame from "./TableCardColumnFrame";
-import TableCard from "./TableCard";
-import TableCardDrawpile from "./TableCardDrawpile";
 
 export {
     TableCardFrame,
     TableCardRowFrame,
     TableCardColumnFrame,
-    TableCard,
-    TableCardDrawpile,
 };
