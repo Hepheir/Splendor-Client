59da9e11cadbebc1f8f554ce88f68c64deda59ed
Hepheir <hepheir@gmail.com>
2021-11-12 22:22:11 +0900

    components/TableCard: TableCard의 중앙 정렬이 이상하던 문제를 수정

--- src/components/TableCard/TableCardRowFrame/index.jsx
+++ src/components/TableCard/TableCardRowFrame/index.jsx
@@ -4,8 +4,8 @@ import "./style.css";
 function TableCardRowFrame(props) {
     const { children } = props;
     return (
-        <div className="table-card-row-frame-container">
-            <div className="table-card-row-frame">
+        <div className="table-card-row-frame">
+            <div className="table-card-row-frame-container">
                 {children}
             </div>
         </div>
